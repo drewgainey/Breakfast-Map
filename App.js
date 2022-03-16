af44c3f1838c24f5e554caedd925b6b6cf5fda81
Drew Gainey <drewgainey@gmail.com>
2022-03-16 12:08:29 -0400

    updated to set inital view to users location

--- App.js
+++ App.js
@@ -2,28 +2,56 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import MapView, { Callout, Marker } from "react-native-maps";
 import resturants from "./resturants";
+import * as Location from "expo-location";
+import { useState, useEffect } from "react";
 
 export default function App() {
+  const [currentLatitude, setCurrentLatitude] = useState(null);
+  const [currentLongitude, setCurrentLongitude] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      let curLatitude = location.coords.latitude;
+      let curLongitude = location.coords.longitude;
+      setCurrentLatitude(curLatitude);
+      setCurrentLongitude(curLongitude);
+      setIsLoading(false);
+    })();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <MapView
-        initialRegion={{
-          latitude: 40.72422,
-          longitude: -73.98317,
-          latitudeDelta: 0.030733,
-          longitudeDelta: 0.014033,
-        }}
-        style={styles.map}
-      >
-        {resturants.map((resturant) => (
-          <Marker key={resturant.name} coordinate={resturant.location}>
-            <Callout>
-              <Text>{resturant.name}</Text>
-              <Text>{resturant.hours}</Text>
-            </Callout>
-          </Marker>
-        ))}
-      </MapView>
+      {isLoading ? (
+        <Text> Waiting...</Text>
+      ) : (
+        <MapView
+          initialRegion={{
+            latitude: currentLatitude,
+            longitude: currentLongitude,
+            latitudeDelta: 0.030733,
+            longitudeDelta: 0.014033,
+          }}
+          style={styles.map}
+        >
+          {resturants.map((resturant) => (
+            <Marker key={resturant.name} coordinate={resturant.location}>
+              <Callout>
+                <Text>{resturant.name}</Text>
+                <Text>{resturant.hours}</Text>
+              </Callout>
+            </Marker>
+          ))}
+        </MapView>
+      )}
     </View>
   );
 }
