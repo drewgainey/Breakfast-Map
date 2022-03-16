50adac52eb0e9c781a5939a07c68ceca70077ecc
Drew Gainey <drewgainey@gmail.com>
2022-03-15 21:36:06 -0400

    created map with sample resturants

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import MapView, { Callout, Marker } from "react-native-maps";
+import resturants from "./resturants";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView
+        initialRegion={{
+          latitude: 40.72422,
+          longitude: -73.98317,
+          latitudeDelta: 0.030733,
+          longitudeDelta: 0.014033,
+        }}
+        style={styles.map}
+      >
+        {resturants.map((resturant) => (
+          <Marker key={resturant.name} coordinate={resturant.location}>
+            <Callout>
+              <Text>{resturant.name}</Text>
+              <Text>{resturant.hours}</Text>
+            </Callout>
+          </Marker>
+        ))}
+      </MapView>
     </View>
   );
 }
@@ -13,8 +31,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  map: {
+    width: "100%",
+    height: "100%",
   },
 });
---
+++ resturants.js
@@ -0,0 +1,12 @@
+export default resturants = [
+  {
+    name: "Fancy NY Grill & Deli",
+    location: { latitude: 40.7228, longitude: -73.98563 },
+    hours: "12:01am - 12:10pm"
+  },
+  {
+    name: "Thompkins Square Bagels",
+    location: { latitude: 40.72825, longitude: -73.98218 },
+    hours: "9:00am - 5:00pm"
+  },
+];
